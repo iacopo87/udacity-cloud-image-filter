e32e7a40f6374b03b07d2986afdcc5ae36fd499b
Iacopo Pazzaglia <iacopo.p.es@sync.money>
2020-05-23 11:13:35 +0200

    feat: configure tslint and resolve erros

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
-import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+import express, { Request, Response } from "express";
+import { deleteLocalFiles, filterImageFromURL } from "./util/util";
 
 (async () => {
   // Init the Express application
@@ -12,23 +12,9 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  //Add an endpoint to GET a specific resource by Primary Key
+  // Add an endpoint to GET a specific resource by Primary Key
   app.get("/filteredimage", async (req: Request, res: Response) => {
-    let { image_url } = req.query;
+    const { image_url } = req.query;
     let filterdImagePath: string;
 
     if (!image_url) {
@@ -45,10 +31,6 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
       .status(200)
       .sendFile(filterdImagePath, () => deleteLocalFiles([filterdImagePath]));
   });
-
-  /**************************************************************************** */
-
-  //! END @TODO1
 
   // Root Endpoint
   // Displays a simple message to the user
